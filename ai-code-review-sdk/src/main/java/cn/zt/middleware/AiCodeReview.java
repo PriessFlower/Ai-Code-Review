1a45738b22ea07a183d3c7b28622b9eb2f20d9c5
墨城有珠 <2837198145@qq.com>
2026-03-20 16:46:11 +0800

    feat: init ai-code-review project
    - setup maven multi-module structure
    - add sdk and test modules
    - configure spring boot 3.5.4 and jdk 21

---
+++ ai-code-review-sdk/src/main/java/cn/zt/middleware/AiCodeReview.java
@@ -0,0 +1,13 @@
+package cn.zt.middleware;
+
+/**
+ * @author 墨城有珠
+ * @version 1.0
+ * @description: TODO
+ * @date 2026/3/20 15:50
+ */
+public class AiCodeReview {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ ai-code-review-test/src/test/java/cn/zt/middleware/ApiTest.java
@@ -0,0 +1,23 @@
+package cn.zt.middleware;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 墨城有珠
+ * @version 1.0
+ * @description: TODO
+ * @date 2026/3/20 16:33
+ */
+@Slf4j
+@SpringBootTest
+public class ApiTest {
+
+    @Test
+    public void test() {
+        System.out.println(Integer.parseInt("111"));
+    }
+
+}
